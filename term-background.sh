dfb52cf06bb92fd58ec93932093fc3c1778a357a
rocky <rocky@gnu.org>
2019-09-01 20:58:05 -0400

    Add GPL 2+ license

--- term-background.sh
+++ term-background.sh
@@ -2,6 +2,22 @@
 # ^^^^^^^^^^^^ Use env rather than assume we've got /bin/bash.
 # This works better on OSX where /bin/bash is brain dead.
 
+#   Copyright (C) 2019, Rocky Bernstein <rocky@gnu.org>
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation; either version 2, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; see the file COPYING.  If not, write to
+#   the Free Software Foundation, 59 Temple Place, Suite 330, Boston,
+#   MA 02111 USA.
+
 # Try to determine if we have dark or light terminal background
 
 typeset -i success=0
