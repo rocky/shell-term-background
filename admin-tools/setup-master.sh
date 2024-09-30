8977681839da244869e22b4a87acd9c57c955f3e
rocky <rocky@gnu.org>
2024-09-30 16:31:14 -0400

    Administrivia

---
+++ admin-tools/setup-master.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PYTHON_VERSION=3.12
+pyenv local $PYTHON_VERSION
+
+if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
+git checkout master && git pull && pyenv local $PYTHON_VERSION
