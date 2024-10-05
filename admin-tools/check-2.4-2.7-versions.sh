ffcbcef421d02438f372685dd28f0b1bbadd660b
rocky <rocky@gnu.org>
2024-10-05 07:30:15 -0400

    More administrivia

---
+++ admin-tools/check-2.4-2.7-versions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+check_term_background_24_owd=$(pwd)
+
+cd $(dirname ${BASH_SOURCE[0]})
+if ! source ./pyenv-2.4-2.7-versions ; then
+    exit $?
+fi
+if ! source ./setup-python-2.4.sh ; then
+    exit $?
+fi
+cd ..
+for version in $PYVERSIONS; do
+    echo --- $version ---
+    if ! pyenv local $version ; then
+	exit $?
+    fi
+    make clean && pip install -e .
+    if ! make check; then
+	exit $?
+    fi
+    echo === $version ===
+done
+cd $check_term_background_24_owd
