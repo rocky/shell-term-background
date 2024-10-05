15bc72033c1dbcddf3f78833809b95ae8813c56b
rocky <rocky@gnu.org>
2024-10-05 16:48:43 -0400

    Merge hell

--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-PYTHON_VERSION=2.4.6
+PYTHON_VERSION=3.0
+pyenv local $PYTHON_VERSION
 
-owd=$(pwd)
-bs=${BASH_SOURCE[0]}
-if [[ $0 == $bs ]] ; then
-    echo "This script should be *sourced* rather than run directly through bash"
-    exit 1
-fi
-
-mydir=$(dirname $bs)
-fulldir=$(readlink -f $mydir)
-cd $fulldir/..
-
-(cd ../python-xdis && git checkout python-2.4-to-2.7 && pyenv local $PYTHON_VERSION) && git pull && \
-    git checkout python-2.4  && git pull && pyenv local $PYTHON_VERSION
-
-cd $owd
 git checkout python-2.4-to-2.7 && git pull && pyenv local $PYTHON_VERSION
-rm -v */.python-version 2>/dev/null || true
