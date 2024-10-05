b7b9ed1ae5f207866cd74e454a5d18688eeb3b8e
rocky <rocky@gnu.org>
2024-10-05 07:30:15 -0400

    More administrivia

--- admin-tools/setup-python-3.0.sh
+++ admin-tools/setup-python-3.0.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 PYTHON_VERSION=3.0
+pyenv local $PYTHON_VERSION
 
-python_termbackground_owd=$(pwd)
-bs=${BASH_SOURCE[0]}
-if [[ $0 == $bs ]] ; then
-    echo "This script should be *sourced* rather than run directly through bash"
-    exit 1
-fi
-
-mydir=$(dirname $bs)
-fulldir=$(readlink -f $mydir)
-cd $fulldir/..
-
-cd $python_termbackground_owd
-git checkout python-3.0-to-3.2
-rm -v */.python-version 2>/dev/null || true
+git checkout python-3.0-to-3.2 && git pull && pyenv local $PYTHON_VERSION
