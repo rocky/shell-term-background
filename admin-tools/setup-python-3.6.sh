5e04a721bea0fef285f41db0da1157c6ab9945c0
rocky <rocky@gnu.org>
2025-01-13 09:20:09 -0500

    Put 3.11 in the branch with 3.6
    3.11 build works with setuptools

--- admin-tools/setup-python-3.6.sh
+++ admin-tools/setup-python-3.6.sh
@@ -2,4 +2,4 @@
 PYTHON_VERSION=3.6
 pyenv local $PYTHON_VERSION
 
-git checkout python-3.6-to-3.10 && git pull && pyenv local $PYTHON_VERSION
+git checkout python-3.6-to-3.11 && git pull && pyenv local $PYTHON_VERSION
